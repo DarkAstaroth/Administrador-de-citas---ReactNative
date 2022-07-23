f8ed9c7e4b954f114c514d7c84f59757d887a80a
Manuel Ninahuanca <mninahuanca4@gmail.com>
2022-07-23 12:03:32 -0400

    Primer commit

---
+++ App.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  Button,
+  Pressable,
+  Modal
+} from 'react-native';
+
+
+const App = () => {
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const nuevaCitaHandler = () => {
+    console.log('diste click ...')
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titulo}>Administrador de citas {''}
+        <Text style={styles.tituloBold}>Veterinaria</Text>
+      </Text>
+      <Pressable
+        onPress={() => setModalVisible(true)}
+        style={styles.btnNuevaCita}
+      >
+        <Text style={styles.btnTextoNuevaCita}>Nueva Cita</Text>
+      </Pressable>
+
+      <Modal
+        animationType='slide'
+        visible={modalVisible}
+      >
+        <Text>Desde Modal</Text>
+      </Modal>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f3f4f6',
+    flex: 1
+  },
+  titulo: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#374151',
+    fontWeight: '600'
+  },
+  tituloBold: {
+    fontWeight: '900',
+    color: '#6d28d9'
+
+  },
+  btnNuevaCita: {
+    backgroundColor: '#6d28d9',
+    padding: 15,
+    marginTop: 30,
+    marginHorizontal: 20,
+    borderRadius: 10
+  },
+  btnTextoNuevaCita: {
+    textAlign: 'center',
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight: '900',
+    textTransform: 'uppercase'
+  }
+})
+
+
+
+export default App;
